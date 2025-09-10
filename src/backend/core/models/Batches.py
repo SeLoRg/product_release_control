35def4963fdb0a07eff17ee5e434d6f26f0215e0
rosti <rostislavmarinov@bk.ru>
2025-09-10 23:24:01 +0300

    dev: change models

--- src/backend/core/models/Batches.py
+++ src/backend/core/models/Batches.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import mapped_column, Mapped, relationship
-from sqlalchemy import ForeignKey, String, Integer, UniqueConstraint
+from sqlalchemy import ForeignKey, String, UniqueConstraint
 from src.backend.core.models import Base, ShiftTasks, Products
 import datetime
 
@@ -7,20 +7,16 @@ import datetime
 class Batches(Base):
     __tablename__ = "batches"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     shift_task_id: Mapped[int] = mapped_column(
         ForeignKey("shift_tasks.id"), nullable=False
     )
-
     batch_number: Mapped[int] = mapped_column(nullable=False)
     batch_date: Mapped[datetime.date] = mapped_column(nullable=False)
     nomenclature: Mapped[str] = mapped_column(String(128), nullable=False)
     ekn_code: Mapped[str] = mapped_column(String(64), nullable=False)
 
     __table_args__ = (
-        UniqueConstraint(
-            "shift_task_id", "batch_number", "batch_date", name="uq_shift_batch"
-        ),
+        UniqueConstraint("batch_number", "batch_date", name="uq_batch_number_date"),
     )
 
     # Связи
--- src/backend/core/models/WorkCenters.py
+++ src/backend/core/models/WorkCenters.py
@@ -7,7 +7,6 @@ class WorkCenters(Base):
     __tablename__ = "work_centers"
 
     name: Mapped[str] = mapped_column(String(128), nullable=False)
-    code: Mapped[str] = mapped_column(String(64), nullable=False, unique=True)
 
     shift_tasks: Mapped[list["ShiftTasks"]] = relationship(
         "ShiftTasks", back_populates="work_center"
